97f7ef40b2b2f91936658228eff0cbb6a7e1dc8e
Jaswanth <jaswacse2k22@gmail.com>
2024-08-19 09:49:30 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -183,30 +183,55 @@ import { useState,useEffect } from 'react';
 
 // ReactDOM.render(<div><Welcome /></div>,document.getElementById("root"));
 
-function ExampleRender(){
-    const [count,setCount] = useState(0);
+// function ExampleRender(){
+//     const [count,setCount] = useState(0);
 
-    useEffect(()=>{
+//     useEffect(()=>{
         
-    setTimeout(() =>{
-        setCount(count =>count+1)
-    },1000)
-}
-);
+//     setTimeout(() =>{
+//         setCount(count =>count+1)
+//     },1000)
+// }
+// );
 
-return(
-    <h1>LOVE You {count} times</h1>
-)
-}
-
-ReactDOM.render(<ExampleRender />,document.getElementById("root"));
-
-
-// function Userdemo(){
-//     const [users,setUser] = useState();
-//     const [loading.setLoading] = useState[0];
+// return(
+//     <h1>LOVE You {count} times</h1>
+// )
 // }
 
-// fetch('https://jsonplaceholder.typicode.com/todos/1')
-//       .then(response => response.json())
-//       .then(json => console.log(json))
+// ReactDOM.render(<ExampleRender />,document.getElementById("root"));
+
+
+function Userdemo(){
+    const [users,setUsers] = useState([]);
+    const [loading,setLoading] = useState(true);
+
+useEffect(()=>{
+fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data =>{
+      setUsers(data);
+      setLoading(false);
+      })
+    .catch (error =>{
+        console.error("if it is not loading will be error message");
+
+        setLoading(false);
+    });
+},[]);
+    if(loading){
+        return <p>Loading.....</p>;
+    }
+
+    return(
+        <div>
+        <h1>List out the Users in API</h1>
+        <ul>
+            {users.map(user => (
+                <li key={user.id}>{user.name} Email.id:{user.email}</li>
+            ))}
+        </ul>
+        </div>
+    );
+}
+ReactDOM.render(<Userdemo />,document.getElementById("root")) ;
